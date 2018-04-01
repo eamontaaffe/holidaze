bb30d43e75f11da8900bd1c388177190c387fb74
Eamon Taaffe <eamontaaffe@gmail.com>
2018-04-01 17:18:10 +0200

    Added pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 const _ = require("lodash")
-/* const Promise = require("bluebird")*/
+const fs = require(`fs-extra`)
+const createPaginatedPages = require("gatsby-paginate")
 const path = require("path")
 const select = require(`unist-util-select`)
-const fs = require(`fs-extra`)
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
@@ -13,28 +13,55 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     resolve(
       graphql(
         `
-      {
-        allMarkdownRemark(
-          limit: 1000
-        ) {
-          edges {
-            node {
-              frontmatter {
-                path
+query GatsbyNodeQuery {
+  allMarkdownRemark(
+    sort: {fields: [frontmatter___date],
+    order: DESC}, filter: {frontmatter: {hidden: {ne: true}}}
+  ) {
+    edges {
+      node {
+        id
+        frontmatter {
+          path
+          date(formatString: "DD.MM.YY")
+          title
+          image {
+            childImageSharp {
+              resolutions(width: 360, height: 360) {
+                base64
+                width
+                height
+                src
+                srcSet
               }
             }
           }
         }
       }
+    }
+  }
+}
+
     `
       ).then(result => {
         if (result.errors) {
-          console.log(result.errors)
+          console.error(result.errors)
           reject(result.errors)
         }
 
+        // Get all post edges
+        const edges = result.data.allMarkdownRemark.edges;
+
+        // Create paginated index pages.
+        createPaginatedPages({
+          edges,
+          createPage,
+          pageTemplate: "src/templates/index.js",
+          pageLength: 9,
+        });
+
         // Create blog posts pages.
-        _.each(result.data.allMarkdownRemark.edges, edge => {
+        _.each(edges, edge => {
           createPage({
             path: edge.node.frontmatter.path,
             component: blogPost,
