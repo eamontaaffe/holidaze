eecb1bcf358dd4e3c3d96215eea7bb460c3ccfc5
Eamon Taaffe <eamontaaffe@gmail.com>
2018-05-07 19:49:06 +0200

    Added subscribe component and styling
    Next step is to hook it up to a back end system.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
-import './index.css'
+import './index.css';
+
+const Subscription = () => (
+  <div className="footer">
+    <form className="subscription">
+      <input type="text" placeholder="Email"/>
+      <button>Subscribe</button>
+    </form>
+    <div>
+      <h3 className="copywrite">
+  &copy; All images are copyright of <a href= "http://www.florenceltf.com">Florence Li Ting Fong</a> and <a href="https://eamontaaffe.github.io">Eamon Taaffe</a>.
+      </h3>
+    </div>
+  </div>
+);
 
 const TemplateWrapper = ({ children, data }) => (
   <div className="template">
     <h1><Link to="/">{data.site.siteMetadata.title}</Link></h1>
     {children()}
-    <h3 className="footer">
-      &copy; All images are copyright of <a href= "http://www.florenceltf.com">Florence Li Ting Fong</a> and <a href="https://eamontaaffe.github.io">Eamon Taaffe</a>.
-    </h3>
+    <Subscription />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
 
 export const query = graphql`
   query TemplateQuery {
@@ -28,4 +40,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
