7650fb4e6c39c1445db2b219e1c8c5e585171eb5
Eamon Taaffe <eamontaaffe@gmail.com>
2018-05-08 19:12:44 +0200

    Subscription hooked up to azure function

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-
 import './index.css';
 
-const Subscription = () => (
+const AZURE_CODE = 'GlW01LTr9nBaPo1w9z3y59GCNq9Q9Wy652Fllm51l7pn4/8q83ep6Q=='
+const SUBSCRIBE_URI = 'https://holidaze.azurewebsites.net/api/subscribe';
+
+class Subscription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      subscribing: false,
+      placeholder: 'Your Email',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  handleSubmit(event) {
+    console.log(`Subscribe: ${this.state.email}`);
+    event.preventDefault();
+
+    this.setState({
+      ...this.state,
+      subscribing: true,
+    });
+
+    const email = encodeURIComponent(this.state.email);
+    const uri = `${SUBSCRIBE_URI}?code=${AZURE_CODE}&email=${email}`;
+
+    fetch(uri)
+      .then(({ ok }) => {
+        if(ok) {
+          this.setState({
+            email: '',
+            subscribing: false,
+            placeholder: 'You have subscribed!',
+          });
+          document.activeElement.blur();
+        } else {
+          this.setState({
+            email: '',
+            subscribing: false,
+            placeholder: 'Issue subscribing',
+          });
+        }
+      })
+  }
+
+  render() {
+    return (
+      <form className="subscription" onSubmit={this.handleSubmit}>
+        <fieldset disabled={this.state.subscribing}>
+          <input
+            type="text"
+            placeholder={this.state.placeholder}
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Subscribe</button>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+const Footer = () => (
   <div className="footer">
-    <form className="subscription">
-      <input type="text" placeholder="Email"/>
-      <button>Subscribe</button>
-    </form>
+    <Subscription />
     <div>
       <h3 className="copywrite">
   &copy; All images are copyright of <a href= "http://www.florenceltf.com">Florence Li Ting Fong</a> and <a href="https://eamontaaffe.github.io">Eamon Taaffe</a>.
@@ -22,7 +86,7 @@ const TemplateWrapper = ({ children, data }) => (
   <div className="template">
     <h1><Link to="/">{data.site.siteMetadata.title}</Link></h1>
     {children()}
-    <Subscription />
+    <Footer />
   </div>
 );
 
