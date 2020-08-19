d6b972575e9039667cdf1d6caa6bee6ff60be6d2
Florence Li <florenceli@Florences-MBP.gateway>
2020-08-19 17:53:28 +1000

    mobile cursor image display none

--- static/js/section.js
+++ static/js/section.js
@@ -7,25 +7,30 @@ function moveCursor(event) {
         + (event.clientX + 10) + "px," + (event.clientY + 20) + "px,0px"
 }
 
-for (let country of countryList) {
-    country.addEventListener('mousemove', moveCursor)
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    cursorImage.style.display = "none"
+} else {
 
-    country.addEventListener('mouseenter', function (event) {
-        for (let img of cursorImage.children) {
-            if (img.dataset.country === event.target.dataset.country) {
-                img.style.display = "block"
-            } else {
+    for (let country of countryList) {
+        country.addEventListener('mousemove', moveCursor)
+
+        country.addEventListener('mouseenter', function (event) {
+            for (let img of cursorImage.children) {
+                if (img.dataset.country === event.target.dataset.country) {
+                    img.style.display = "block"
+                } else {
+                    img.style.display = "none"
+                }
+            }
+            moveCursor(event);
+        });
+
+        country.addEventListener('mouseleave', function (event) {
+            const country = event.target.dataset.country;
+            for (let img of cursorImage.children) {
                 img.style.display = "none"
             }
-        }
-        moveCursor(event);
-    });
-
-    country.addEventListener('mouseleave', function (event) {
-        const country = event.target.dataset.country;
-        for (let img of cursorImage.children) {
-            img.style.display = "none"
-        }
-        moveCursor(event);
-    });
+            moveCursor(event);
+        });
+    }
 }
